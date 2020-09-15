b8a8cc30454d8e38239cd7bd59db9ec261acfe59
erinmayg <erinmaygunawan@gmail.com>
2020-09-15 16:00:35 +0800

    Create landing page

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import Button from './components/button';
+// import Button from './components/button';
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import About from './components/About';
+import Review from './components/Review';
 
 function App() {
+  
   const [foo, setFoo] = useState('N/A');
   useEffect(() => {
     fetch('/api/foo')
@@ -9,17 +14,25 @@ function App() {
       .then((data) => setFoo(data.foo))
       .catch((err) => setFoo(err.message));
   }, []);
+
   return (
-    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-      <h1>Hello World</h1>
-      <p>
-        Server responded with foo:
-        {foo}
-      </p>
-      <Button onClick={() => console.log('I was clicked')}>
-        I am a button
-      </Button>
-    </div>
+    // <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+    //   <h1>Hello World</h1>
+    //   <p>
+    //     Server responded with foo:
+    //     {foo}
+    //   </p>
+    //   <Button onClick={() => console.log('I was clicked')}>
+    //     I am a button
+    //   </Button>
+    // </div>
+
+    <div className="app">
+      <Nav />
+      <Landing />
+      <About />
+      <Review />
+    </div> 
   );
 }
 
---
+++ packages/front/src/components/Review.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../css/review.css";
+
+function Review() {
+  return (
+    <div className="review">
+      <div
+        className="review__content"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1600/https://www.clubpets.com.sg/wp-content/uploads/2019/08/pet-hacks-5-dog-breeds-unsuitable-for-first-time-pet-owners-2.jpg)`,
+          backgroundPosition: "center center",
+        }}
+      ></div>
+
+      <div className="review__content--text">
+        <hr></hr>
+        <h1>
+          My PetSos caretaker sent me updates throughout the day and took care
+          of my dog as if she were her own. <span className="quote">– Florence</span>
+        </h1>
+        <h2>Singapore, SG</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Review;

731b7611718bfb4fcd2aa3090982f518822faf32
Florencia Martina <florenciamartina15@gmail.com>
2020-09-16 11:23:40 +0800

    Change SignUpPage path to /signup

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -1,31 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import React, { useState, useEffect } from "react";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [foo, setFoo] = useState('N/A');
+  const [foo, setFoo] = useState("N/A");
   useEffect(() => {
-    fetch('/api/foo')
+    fetch("/api/foo")
       .then((res) => res.json())
       .then((data) => setFoo(data.foo))
       .catch((err) => setFoo(err.message));
   }, []);
   return (
-
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/signup">
             <SignUpPage />
           </Route>
-          
+
           <Route exact path="/login">
             <LoginPage />
           </Route>

cd79613374df87b2d8c92503779f25253f6ab09a
erinmayg <erinmaygunawan@gmail.com>
2020-09-24 02:56:40 +0800

    Make responsive

--- packages/front/src/components/AddPet.jsx
+++ packages/front/src/components/AddPet.jsx
@@ -1,29 +1,56 @@
 import React, { useState } from "react";
 import "../css/addPet.css";
+import logo from "../resources/dogs-cats-header.png";
 
 function AddPet() {
+  const [showOther, setShowOther] = useState(false);
+  const [selectType, setSelectType] = useState(false);
+
   return (
     <div>
+      <img
+        // src="https://blog.verisure.co.uk/wp-content/uploads/2018/12/shutterstock_1034939470-2000x761.jpg"
+        src={logo}
+        width="100%"
+      />
       <section class="flex justify-center p-6">
-        <div class="w-60 m-auto p-6 w-1/2">
+        <div class="md:w-1/2 w-full p-6 border rounded bg-white shadow">
           <h1 class="font-bold">Tell us a bit about your pet.</h1>
           <h2 class="mb-4">What type of pet?</h2>
-          <div class="text-center mb-4">
-            <button>
+          <div class="text-center">
+            <button
+              class="m-2"
+              onClick={() => {
+                setShowOther(false);
+                setSelectType(true);
+              }}
+            >
               <img
                 alt="Dog"
                 src="https://www.flaticon.com/svg/static/icons/svg/3135/3135506.svg"
               />
               <h3 class="mt-2">Dog</h3>
             </button>
-            <button>
+            <button
+              class="m-2"
+              onClick={() => {
+                setShowOther(false);
+                setSelectType(true);
+              }}
+            >
               <img
                 alt="Cat"
                 src="https://www.flaticon.com/svg/static/icons/svg/3135/3135379.svg"
               />
               <h3 class="mt-2">Cat</h3>
             </button>
-            <button>
+            <button
+              class="m-2"
+              onClick={() => {
+                setShowOther(true);
+                setSelectType(true);
+              }}
+            >
               <img
                 alt="Others"
                 src="https://www.flaticon.com/svg/static/icons/svg/3135/3135369.svg"
@@ -31,24 +58,47 @@ function AddPet() {
               <h3 class="mt-2">Others</h3>
             </button>
           </div>
+
+          {showOther ? (
+            <div class="mt-4">
+              <h1 class="font-semibold">Pet Category</h1>
+              <input
+                type="text"
+                name="Pet Category"
+                placeholder="Pet Category"
+              />
+            </div>
+          ) : null}
         </div>
       </section>
 
       <section class="flex justify-center p-6">
-        <div class="w-60 m-auto p-6 w-1/2">
-          <div class="flex">
-            <label class="mr-8">
+        <div class="md:w-1/2 w-full p-6 border rounded bg-white shadow">
+          <div class="lg:flex md:block">
+            <div class="mr-8 mb-2">
               <h1 class="font-semibold">Pet Name</h1>
               <input type="text" name="Pet Name" placeholder="Pet Name" />
-            </label>
-            <label>
-              <h1 class="font-semibold">Sex</h1>
+            </div>
+            <div class="mb-2">
+              <h1 class="font-semibold mb-1">Sex</h1>
               <input type="radio" value="Male" name="gender" /> Male
-              <input type="radio" value="Female" name="gender" /> Female
-              <input type="radio" value="Other" name="gender" /> Other / NA
-            </label>
+              <input
+                type="radio"
+                value="Female"
+                name="gender"
+                class="ml-2 mr-1"
+              />
+              Female
+              <input
+                type="radio"
+                value="Other"
+                name="gender"
+                class="ml-2 mr-1"
+              />
+              Other / NA
+            </div>
           </div>
-          <div>
+          <div class="mb-2">
             <h1 class="font-semibold">Breed</h1>
             <select>
               <option value="">Hoho</option>
@@ -57,18 +107,18 @@ function AddPet() {
               <option value="">Hoho</option>
             </select>
           </div>
-          <div className="petSize">
+          <div>
             <h1 class="font-semibold">Size</h1>
             <h2>Choose your pet size!</h2>
             <div class="text-center">
-              <button>
+              <button class="m-2 md:w-32 w-24 px-8 py-4">
                 <img
                   alt="Small"
                   src="https://www.flaticon.com/svg/static/icons/svg/2881/2881761.svg"
                 />
                 <h3 class="mt-2">Small</h3>
               </button>
-              <button>
+              <button class="m-2 md:w-40 w-32 px-8 py-4">
                 <img
                   alt="Medium"
                   src="https://www.flaticon.com/svg/static/icons/svg/2965/2965396.svg"
@@ -76,7 +126,7 @@ function AddPet() {
                 />
                 <h3 class="mt-2">Medium</h3>
               </button>
-              <button>
+              <button class="m-2 md:w-48 px-8 py-4">
                 <img
                   alt="Large"
                   src="https://www.flaticon.com/svg/static/icons/svg/91/91544.svg"
@@ -89,7 +139,7 @@ function AddPet() {
       </section>
 
       <section class="flex justify-center p-6">
-        <div class="w-1/2 p-6">
+        <div class="md:w-1/2 p-6 border rounded bg-white shadow">
           <label>
             <h1 class="font-semibold mb-2">Special requirements</h1>
             <textarea
@@ -112,6 +162,11 @@ function AddPet() {
           <button class="cancel-button">Cancel</button>
         </div>
       </section>
+      {/* <img
+        src="https://www.twentyfifthhourconcierge.com/wp-content/uploads/2017/10/pet-sitting-header.jpg"
+        width="100%"
+      /> */}
+      {/* <img src="https://www.pouncepetinsurance.com.au/wp-content/uploads/2018/10/header-cover.jpg" /> */}
     </div>
   );
 }

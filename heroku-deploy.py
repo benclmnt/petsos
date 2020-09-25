b3f6a604df5ce5a0e8e46a7649726806be9f9cca
benclmnt <49342399+benclmnt@users.noreply.github.com>
2020-09-26 00:27:27 +0800

    parent b1841dfcc4fefefe0b7b94838d28bcd72e3f729d author benclmnt <49342399+benclmnt@users.noreply.github.com> 1601051247 +0800 committer Bennett Clement <benclmnt.dev@gmail.com> 1601078193 +0800
    Deploy to Heroku (#11)
    
    * Commit from heroku-deploy.py for deployment

--- heroku-deploy.py
+++ heroku-deploy.py
@@ -8,14 +8,13 @@ def init_deploy():
     shutil.rmtree(os.path.join(FILE_PATH, 'web'))
 
 def build_backend():
-    os.system('yarn build:back')
+    os.system('(cd packages/back && yarn && yarn build:heroku)')
 
 def copy_backend():
     shutil.copytree(
         os.path.join(FILE_PATH, 'packages', 'back'),
         os.path.join(FILE_PATH, 'web'),
-        ignore=shutil.ignore_patterns('node_modules', 'src',
-            'babel.config.js', '.prettierrc.js', 'tests'))
+        ignore=shutil.ignore_patterns('node_modules', 'src', 'babel.config.js', '.prettierrc.js', '.gitignore', 'tests'))
 
 def build_frontend():
     os.system('yarn build:front')
@@ -26,15 +25,14 @@ def copy_frontend():
         os.path.join(FILE_PATH, 'web', 'front', 'build'))
 
 def push_to_heroku():
+    os.system('git checkout deploy')
     os.system('git add .')
-    os.system('git commit -m "Commit from heroku-deploy.py for \
-    deployment"')
-    os.system('git push origin master')
+    os.system('git commit -m "Commit from heroku-deploy.py for deployment"')
+    os.system('git push origin deploy')
     os.system('git subtree push --prefix web heroku master')
 
 def main():
     init_deploy()
-    build_backend()
     copy_backend()
     build_frontend()
     copy_frontend()
---
+++ web/build/routes/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getRoutes = getRoutes;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _users = require("./users");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function getRoutes() {
+  const router = _express.default.Router();
+
+  router.use('/users', (0, _users.getUsersRoutes)());
+  return router;
+}

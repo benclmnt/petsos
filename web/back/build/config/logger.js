cd3f5e5d7b796e7ba2669ce0f5d5b80fc8ca5e11
Bennett Clement <benclmnt.dev@gmail.com>
2020-09-26 00:20:41 +0800

    Commit from heroku-deploy.py for     deployment

---
+++ web/back/build/config/logger.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  logLevel: process.env.LOG_LEVEL || 'info'
+};
+exports.default = _default;

6ac1decea9150978b2e4750088f069b7b8c8b843
Bennett Clement <benclmnt.dev@gmail.com>
2020-09-25 23:32:57 +0800

    Commit from heroku-deploy.py for     deployment

--- heroku-deploy.py
+++ heroku-deploy.py
@@ -7,14 +7,11 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 def init_deploy():
     shutil.rmtree(os.path.join(FILE_PATH, 'web'))
 
-def build_backend():
-    os.system('yarn build:back')
-
 def copy_backend():
     shutil.copytree(
         os.path.join(FILE_PATH, 'packages', 'back'),
         os.path.join(FILE_PATH, 'web'),
-        ignore=shutil.ignore_patterns('node_modules', 'src',
+        ignore=shutil.ignore_patterns('node_modules', 'build'
             'babel.config.js', '.prettierrc.js', 'tests'))
 
 def build_frontend():
@@ -29,12 +26,11 @@ def push_to_heroku():
     os.system('git add .')
     os.system('git commit -m "Commit from heroku-deploy.py for \
     deployment"')
-    os.system('git push origin master')
+    # os.system('git push origin master')
     os.system('git subtree push --prefix web heroku master')
 
 def main():
-    init_deploy()
-    build_backend()
+    # init_deploy()
     copy_backend()
     build_frontend()
     copy_frontend()
---
+++ web/src/config/db.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT || 5432,
+  database: process.env.PG_DB,
+};

b43870fb9935eaa6099748353dd35def52c92c90
Bennett Clement <benclmnt.dev@gmail.com>
2020-09-25 18:41:19 +0800

    Implement login user

--- packages/back/src/routes/users.js
+++ packages/back/src/routes/users.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import logger from '../logger';
 import { query } from '../db';
-import { getAllUsers, registerUser } from '../db/queries';
+import {
+  getAllUsers,
+  registerUser,
+  queryUserById,
+  queryUserByEmail,
+} from '../db/queries';
 
 function getUsersRoutes() {
   const router = express.Router();
-  router.get('/', listAllUsers);
-  router.get('/:userId', getUserById);
   router.post('/login', login);
   router.post('/register', register);
+  router.get('/:userId', getUserById);
+  router.get('', listAllUsers);
   return router;
 }
 
@@ -22,20 +27,38 @@ async function register(req, res) {
     logger.error(err);
     return buildUsersErrorObject(res, {
       status: 400,
-      error: err,
+      error: 'Email already registered.',
     });
   }
 
-  const user = await getUserInfo();
-  return res.json(body);
+  const user = await query(queryUserByEmail, [body.email]);
+  return buildUsersSuccessObject(res, { user });
 }
 
-async function login(req, res) {}
+async function login(req, res) {
+  const { body } = req;
+  const users = await query(queryUserByEmail, [body.email]);
+
+  checkUserExists(res, users);
+
+  if (body.password !== users[0].password) {
+    return buildUsersErrorObject(res, {
+      status: 401,
+      error: 'Wrong email or password',
+    });
+  }
+
+  return buildUsersSuccessObject(res, { user: users[0] });
+}
 
 async function getUserById(req, res) {
-  const user = await query(getUserInfo, params);
+  // TODO: parse the userId. Make sure it is number
+  const users = await query(queryUserById, [req.params.userId]);
+
+  checkUserExists(res, users);
+
   return buildUsersSuccessObject(res, {
-    user,
+    user: users[0],
   });
 }
 
@@ -62,4 +85,13 @@ function buildUsersErrorObject(res, { status, error }) {
 
 function buildUsersSuccessObject(res, { status, user }) {
   return res.status(status || 200).json(user);
+}
+
+function checkUserExists(res, users) {
+  if (users.length === 0) {
+    return buildUsersErrorObject(res, {
+      status: 400,
+      error: 'Cannot find user',
+    });
+  }
 }

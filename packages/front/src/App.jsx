4ba5dc9669bb44be7e852bcc54741d020dc2e6ab
drake25122000 <60537545+drake25122000@users.noreply.github.com>
2020-09-26 17:02:54 +0800

    Design edit profile page V1 (#7)
    * Design edit profile pagge V1
    
    * cleanup
    
    Co-authored-by: Bennett Clement <benclmnt.dev@gmail.com>

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
-import LoginPage from "./components/LoginPage";
-import SignUpPage from "./components/SignUpPage";
-import SearchSitters from "./components/SearchSitters";
-import Nav from "./components/Nav"
-import Landing from "./components/landing/Landing"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
+import SearchSitters from './components/SearchSitters';
+import Nav from './components/Nav';
+import Landing from './components/landing/Landing';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import EditProfile from './components/editprofile/EditProfile';
 
 function App() {
   const [foo, setFoo] = useState('N/A');
@@ -26,6 +27,8 @@ function App() {
             <LoginPage />
           </Route>
 
+          <Route path="/profile/edit" component={EditProfile} />
+
           <Route exact path="/searchsitters">
             <SearchSitters />
           </Route>
@@ -34,7 +37,6 @@ function App() {
             <Nav />
             <Landing />
           </Route>
-          
         </Switch>
       </Router>
     </div>

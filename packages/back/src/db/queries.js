352f316759374acc4f2fde36a42c81e0c3519c5f
Florencia Martina <florenciamartina15@gmail.com>
2020-10-18 18:54:05 +0800

    Add schema to init.sql

--- packages/back/src/db/queries.js
+++ packages/back/src/db/queries.js
@@ -1,7 +1,10 @@
+// User-related queries
 export const getAllUsers = 'SELECT * FROM users LIMIT 25;';
 export const queryUserByUsername = 'SELECT * FROM users WHERE username = $1;';
 export const queryUserByEmail = 'SELECT * FROM users WHERE email = $1;';
 export const registerUser =
   'INSERT INTO users(username, email, password) VALUES ($1, $2, $3);';
 export const upsertUserAddress =
-  'UPDATE users SET address = $2, city = $3, country = $4, postal = $5 WHERE username = $1;';
+  'UPDATE users SET address = $2, city = $3, country = $4, postal_code = $5 WHERE username = $1;';
+
+// Caretaker-related queries

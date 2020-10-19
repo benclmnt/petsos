a167200d6b0360e2ea7bf25a1e493a37aceba6a6
Christian Drake Martin <60537545+drake25122000@users.noreply.github.com>
2020-10-20 00:16:55 +0800

    Add import dashboard

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -6,6 +6,7 @@ import Landing from "./components/landing/Landing";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AddPet from "./components/AddPet";
 import EditProfile from "./components/editprofile/EditProfile";
+import Dashboard from "./components/dashboard/Dashboard";
 import SearchSitters from "./components/SearchSitters"
 
 function App() {

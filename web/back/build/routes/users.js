c1363fd4294ced8ae2ec83a84c58f0b26be644ba
Bennett Clement <benclmnt.dev@gmail.com>
2020-10-20 13:47:09 +0800

    Commit from heroku-deploy.py for deployment

--- web/back/build/routes/users.js
+++ web/back/build/routes/users.js
@@ -20,76 +20,115 @@ function getUsersRoutes() {
 
   router.post('/login', login);
   router.post('/register', register);
-  router.get('/:userId', getUserById);
+  router.get('/:username', getUserByUsername);
+  router.post('/:username/address', upsertUserAddress);
   router.get('', listAllUsers);
   return router;
 }
+/**
+ * Register user using username, email, password
+ */
+
 
 async function register(req, res) {
   const {
-    body
-  } = req;
-  const params = [body.name, body.email, body.password];
+    username,
+    email,
+    password
+  } = req.body;
+  const params = [username, email, password];
+
+  if (checkMissingParameter(params)) {
+    return handleMissingParameter(res);
+  }
 
   try {
     await (0, _db.query)(_queries.registerUser, params);
   } catch (err) {
-    _logger.default.error(err);
-
     return buildUsersErrorObject(res, {
       status: 400,
-      error: 'Email already registered.'
+      error: 'Username already registered.'
     });
   }
 
-  const user = await (0, _db.query)(_queries.queryUserByEmail, [body.email]);
-  return buildUsersSuccessObject(res, {
-    user
+  const user = await (0, _db.query)(_queries.queryUserByEmail, [email]);
+  return buildSuccessResponse(res, {
+    user: buildUsersObject(user)
   });
 }
+/**
+ * Login using email
+ */
+
 
 async function login(req, res) {
   const {
-    body
-  } = req;
-  const users = await (0, _db.query)(_queries.queryUserByEmail, [body.email]);
+    email,
+    password
+  } = req.body;
+
+  if (checkMissingParameter([email, password])) {
+    return handleMissingParameter(res);
+  }
+
+  const users = await (0, _db.query)(_queries.queryUserByEmail, [email]);
   checkUserExists(res, users);
 
-  if (body.password !== users[0].password) {
+  if (password !== users[0].password) {
     return buildUsersErrorObject(res, {
       status: 401,
       error: 'Wrong email or password'
     });
   }
 
-  return buildUsersSuccessObject(res, {
-    user: users[0]
+  return buildSuccessResponse(res, {
+    user: buildUsersObject(users[0])
   });
 }
 
-async function getUserById(req, res) {
+async function getUserByUsername(req, res) {
   const {
-    userId
-  } = req.params; // parse the userId. Return an error if its not a number
+    username
+  } = req.params; // GG SQL INJECTION!
 
-  if (isNaN(userId)) {
-    return buildUsersErrorObject(res, {
-      status: 400,
-      error: 'the API path /users do not support non-numeric userId'
-    });
+  if (checkMissingParameter([username])) {
+    return handleMissingParameter(res);
   }
 
-  const users = await (0, _db.query)(_queries.queryUserById, [req.params.userId]);
+  const users = await (0, _db.query)(_queries.queryUserByUsername, [username]);
   checkUserExists(res, users);
-  return buildUsersSuccessObject(res, {
-    user: users[0]
+  return buildSuccessResponse(res, {
+    user: buildUsersObject(users[0])
   });
 }
 
 async function listAllUsers(req, res) {
-  const users = await (0, _db.query)(_queries.getAllUsers);
-  return buildUsersSuccessObject(res, {
+  let users = await (0, _db.query)(_queries.getAllUsers);
+  users = users.map(buildUsersObject);
+  return buildSuccessResponse(res, {
     user: users
+  });
+}
+
+async function upsertUserAddress(req, res) {
+  const {
+    username,
+    address,
+    city,
+    country,
+    postal
+  } = req.body; // GG SQL INJECTION!
+
+  const params = [username, address, city, country, postal];
+
+  if (checkMissingParameter(params)) {
+    return handleMissingParameter(res);
+  }
+
+  await (0, _db.query)(_queries.upsertUserAddress, params);
+  const users = await (0, _db.query)(_queries.queryUserByUsername, [username]);
+  return buildSuccessResponse(res, {
+    user: buildUsersObject(users[0])
   });
 }
 
@@ -100,17 +139,29 @@ function buildUsersErrorObject(res, {
   status,
   error
 }) {
+  _logger.default.error(error);
+
   const errorResp = {
+    kind: 'Error',
     error
   };
   return res.status(status).json(errorResp);
 }
 
-function buildUsersSuccessObject(res, {
+function buildUsersObject(user) {
+  const obj = {
+    kind: 'User',
+    ...user,
+    selfLink: `/users/${user.username}`
+  };
+  delete obj.password;
+  return obj;
+}
+
+function buildSuccessResponse(res, {
   status,
   user
 }) {
-  delete user.password;
   return res.status(status || 200).json(user);
 }
 
@@ -121,4 +172,15 @@ function checkUserExists(res, users) {
       error: 'Cannot find user'
     });
   }
+}
+
+function checkMissingParameter(array) {
+  return array.some(param => param === undefined || param === null);
+}
+
+function handleMissingParameter(res) {
+  return buildUsersErrorObject(res, {
+    status: 400,
+    error: 'Missing some required parameters'
+  });
 }
--- web/back/build/scripts/migrate.js
+++ web/back/build/scripts/migrate.js
@@ -8,11 +8,14 @@ const config = require('../config/db');
 
 const path = require('path');
 
+let SEED_SCRIPT = path.resolve(__dirname, `./seed.js`);
+let TARGET_FILE = `./migrate-${new Date().toISOString()}.sql`;
+const PATH_TO_TARGET_FILE = path.resolve(__dirname, TARGET_FILE);
 const command = `psql -d ${config.database} -U ${config.user} -p ${config.port} -a -q -f `;
-const initialize = command + path.resolve(__dirname, './init.sql');
+const initialize = command + PATH_TO_TARGET_FILE;
 
 function run(command) {
-  exec(command, (error, stdout, stderr) => {
+  const callback = (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
       return;
@@ -24,7 +27,10 @@ function run(command) {
     }
 
     console.log(`stdout: ${stdout}`);
-  });
+  };
+
+  exec(`node ${SEED_SCRIPT} ${TARGET_FILE}`, callback);
+  exec(command, callback);
 }
 
 run(initialize);

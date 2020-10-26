a34a23d157cf2ac065c7d083e3874fd883c5523b
Florencia Martina <florenciamartina15@gmail.com>
2020-10-26 14:39:03 +0800

    Add basic structure of caretaker card

--- packages/front/src/components/searchcaretakers/CaretakersCard.jsx
+++ packages/front/src/components/searchcaretakers/CaretakersCard.jsx
@@ -4,21 +4,34 @@ import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
 function CaretakersCard() {
-  const [value, setValue] = React.useState(2);
+  const [value, setValue] = React.useState(4.5);
   return (
-    <div class="flex-col max-h-screen max-w-2xl mx-auto">
-      <div class="bg-white text-black px-10 py-8 rounded shadow space-y-3">
-        <div class="flex-col">
-          <div>
-            <h2 class="text-lg text-orange-800">Full Name</h2>
-          </div>
+    <div class="md:flex">
+      <div class="md:flex-shrink-0">
+        <img class="rounded-lg md:w-56" src="" />
+      </div>
+      <div class="mt-4 md:mt-0 md:ml-6">
+        <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+          Full Name
+        </div>
+        <a class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
+          Short bio?
+        </a>
+        <p class="mt-2 text-gray-600">Address of caretaker</p>
 
-          <div>
-            <Box component="fieldset" mb={3} borderColor="transparent">
-              {/* <Typography component="legend">Rating</Typography> */}
-              <Rating name="read-only" value={value} readOnly />
-            </Box>
-          </div>
+        <div class="flex space-x-8 mt-4 mb-2">
+          <a class="block mt-1 text-md leading-tight font-semibold text-gray-900 hover:underline">
+            Fee
+          </a>
+          <a class="block mt-1 text-md leading-tight font-semibold text-gray-900 hover:underline">
+            Commitment level
+          </a>
+        </div>
+        <div>
+          <Box component="fieldset" mb={3} borderColor="transparent">
+            {/* <Typography component="legend">Rating</Typography> */}
+            <Rating name="read-only" precision={0.5} value={value} readOnly />
+          </Box>
         </div>
       </div>
     </div>

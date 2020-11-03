44120c121c63bddddc8257ce9a7bc015753cc33f
erinmayg <erinmaygunawan@gmail.com>
2020-11-04 07:07:26 +0800

    Refine UI

--- packages/front/src/components/admin/AdminHome.jsx
+++ packages/front/src/components/admin/AdminHome.jsx
@@ -1,10 +1,13 @@
-import * as React from 'react';
+import * as React from "react";
 
 function AdminHome() {
   return (
-    <>
-      <h1>Put admin insights here! Go check /api/admin/stats</h1>
-    </>
+    <div className="m-auto">
+      <h1 className="font-semibold text-3xl text-white text-center">
+        Put admin insights here! <br />
+        Go check /api/admin/stats
+      </h1>
+    </div>
   );
 }
 
